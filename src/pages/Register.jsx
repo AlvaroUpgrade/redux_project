f567993d1753974528a000ab0acd3a9ffc6d7594
carlotajuncosa <carlota.juncosa@gmail.com>
2022-11-12 14:26:01 +0100

    FEAT new formulary structure connected to the backend at Register.jsx. Functionality still not checked

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import "./Register.scss";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { newUser } from "../redux/auth/auth.actions";
 import ReusableButton from "../components/ReusableButton";
+import { API } from "../shared/services/api";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm();
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const sendForm = (formularyData) => {
-    const formData = new FormData();
-
-    formData.append("name", formularyData.name);
-    formData.append("email", formularyData.email);
-    formData.append("password", formularyData.password);
-    formData.append("favoriteMovie", formularyData.favoriteMovie);
-    console.log(sendForm);
+  const registerUser = async (formdata) => {
+    dispatch(newUser(formdata, navigate));
   };
 
   return (
-    <form onSubmit={handleSubmit(sendForm)}>
+    <form onSubmit={handleSubmit(registerUser)}>
       <label>
         User name
         <input type="text" {...register("userName", { required: true })} />
@@ -25,29 +29,42 @@ const Register = () => {
       <label>
         Email
         <input
-          type="email"
+          type="text"
+          name="email"
           {...register("email", {
-            required: true,
-            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            required: "Introduce un email, por favor",
+            minLength: {
+              value: 5,
+              message: "El email tiene que ser más largo",
+            },
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "Introduce un email on formato válido",
+            },
           })}
         />
       </label>
+      {errors.email ? (
+        <>
+          {errors.email.type === "required" && <p>{errors.email.message}</p>}
+          {errors.email.type === "minLength" && <p>{errors.email.message}</p>}
+          {errors.email.type === "pattern" && <p>{errors.email.message}</p>}
+        </>
+      ) : null}
       <label>
         Password
         <input
-          type="password"
+          type="password" name="pasword"
           {...register("password", {
-            required: true,
+            required: "Escribe un pasword, por favor",
             pattern:
               /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/,
           })}
         />
       </label>
-     
-      <ReusableButton size="l" color="white" text="ENVIAR">
-      </ReusableButton>
-     
-        
+      {errors.pasword ? <p>El password no es correcto</p> : null}
+
+      <ReusableButton size="l" color="white" text="ENVIAR" disabled={!isValid}></ReusableButton>
     </form>
   );
 };

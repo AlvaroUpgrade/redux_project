4781ade34697dbd732b8d82b05c25934f7a7ebfe
carlotajuncosa <carlota.juncosa@gmail.com>
2022-11-12 18:49:51 +0100

    Switch pairprogramming

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,37 +2,39 @@ import React from "react";
 import "./Navbar.scss";
 import { NavLink } from "react-router-dom";
 import ButtonLogout from "./ButtonLogout";
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
-  const {user, token} = useSelector(state=>state.auth)
+  const { user, token } = useSelector((state) => state.auth);
   return (
     <nav>
       <NavLink to="" activeclassname={"active"}>
         Movies
       </NavLink>
-    {/*   <NavLink to="moviesdetails" activeclassname={"active"}>
+      {/*   <NavLink to="moviesdetails" activeclassname={"active"}>
         Movies
       </NavLink> */}
-      {!user &&
-      <>
-      <NavLink to="login" activeclassname={"active"}>
-        Login
-      </NavLink>
-      <NavLink to="register" activeclassname={"active"}>
-        Register
-      </NavLink>
-      </>
-      }
-      {
-        user &&
+      {!user && (
         <>
-        <NavLink to="/" activeclassname={"active"}>Movies</NavLink>
-        <NavLink to="addmovies" activeclassname={"active"}>Add Movies</NavLink>
-      <ButtonLogout/>
+          <NavLink to="login" activeclassname={"active"}>
+            Login
+          </NavLink>
+          <NavLink to="register" activeclassname={"active"}>
+            Register
+          </NavLink>
         </>
-      }
-      
+      )}
+      {user && (
+        <>
+          <NavLink to="/" activeclassname={"active"}>
+            Movies
+          </NavLink>
+          <NavLink to="addmovies" activeclassname={"active"}>
+            Add Movies
+          </NavLink>
+          <ButtonLogout />
+        </>
+      )}
     </nav>
   );
 };

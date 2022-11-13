fbe0341bd838f2b9a24a0761b1c319c595cad793
AlvaroUpgrade <alvaro.ruiz@bootcamp-upgrade.com>
2022-11-13 18:51:35 +0100

    FEAT: Some style changes

--- src/pages/MoviesHome.jsx
+++ src/pages/MoviesHome.jsx
@@ -16,29 +16,33 @@ const MoviesHome = () => {
   return (
     <div className="containerMovies">
       {isLoading ? (
-        <img className="imageLoading" src="https://www.icegif.com/wp-content/uploads/valentines-icegif-5.gif" alt="loading" />
+        <img
+          className="imageLoading"
+          src="https://www.icegif.com/wp-content/uploads/valentines-icegif-5.gif"
+          alt="loading"
+        />
       ) : !error ? (
         movies.map((movie) => {
           return (
-            
             <div className="movieCard" key={movie._id}>
-              
               <div className="imageContainer">
-              <img className="imageMovie"  src={movie.image} alt={movie.title} />
+                <img
+                  className="imageMovie"
+                  src={movie.image}
+                  alt={movie.title}
+                />
               </div>
               <div className="content">
-              <h3 className="title">{movie.title}</h3>
-              <p className="director"> {movie.director}</p>
-              <ReusableButton
-                size="l"
-                color="yellow"
-                text="VER MÁS"
-                className="verMas"
-              ></ReusableButton>
+                <h3 className="title">{movie.title}</h3>
+                <p className="director"> {movie.director}</p>
+                <ReusableButton
+                  size="l"
+                  color="yellow"
+                  text="VER MÁS"
+                  className="verMas"
+                ></ReusableButton>
               </div>
-              
-              </div>
-           
+            </div>
           );
         })
       ) : (

67a762c6db952bf3b0a23718b0201d5199977027
carlotajuncosa <carlota.juncosa@gmail.com>
2022-11-13 04:23:03 +0100

    FEAT text in PageNotFound component

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const PageNotFound = () => {
   return (
     <div>
+    <h2>ERROR 404</h2>
       <img
         className="cat"
         src="https://media.tenor.com/9zmtHZ0tIjkAAAAj/nyancat-rainbow-cat.gif"
         alt=""
       />
+      <h3>PAGE NOT FOUND</h3>
     </div>
   );
 };
